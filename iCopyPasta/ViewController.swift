752c6773a9f37f3cef63b89c0941b05e2ea9bb0b
vasarhelyia <vasarhelyi.agi@gmail.com>
2016-01-05 19:51:09 +0100

    Initialize project

---
+++ iCopyPasta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCopyPasta
+//
+//  Created by Agnes Vasarhelyi on 05/01/16.
+//  Copyright © 2016 Agnes Vasarhelyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
