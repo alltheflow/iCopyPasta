03629240a6256021764efdd8fd14d0fe71b9c971
vasarhelyia <vasarhelyi.agi@gmail.com>
2016-01-05 21:10:18 +0100

    Add PasteboardService

---
+++ iCopyPasta/PasteboardService.swift
@@ -0,0 +1,14 @@
+//
+//  PasteboardService.swift
+//  iCopyPasta
+//
+//  Created by Agnes Vasarhelyi on 05/01/16.
+//  Copyright © 2016 Agnes Vasarhelyi. All rights reserved.
+//
+
+import UIKit
+
+class PasteboardService {
+    
+    let pasteboard = UIPasteboard.generalPasteboard()
+}
